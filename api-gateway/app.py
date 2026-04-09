b836826e9e0b22fc354620a3475b09c22d3a4d68
Edouard <edouardakotonou@gmail.com>
2026-04-09 08:04:06 -0500

    Fix formatting issues (black)

--- api-gateway/app.py
+++ api-gateway/app.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests
 
 app = FastAPI()
 
+
 @app.get("/track/{id}")
 def track(id: str):
-    return requests.get(f"http://tracking-service:8000/track/{id}").json()
+    response = requests.get(f"http://tracking-service:8000/track/{id}")
+    if response.status_code != 200:
+        raise HTTPException(status_code=500, detail="Tracking failed")
+    return response.json()
+
 
 @app.post("/orders/{id}")
 def create_order(id: str):
-    return requests.post(f"http://order-service:8000/orders/{id}").json()
+    response = requests.post(f"http://order-service:8000/orders/{id}")
+    if response.status_code != 200:
+        raise HTTPException(status_code=500, detail="Order failed")
+    return response.json()

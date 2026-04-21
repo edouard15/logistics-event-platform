55bb3954e9b8867f345596b73237bc68493269a5
Edouard <edouardakotonou@gmail.com>
2026-04-21 00:33:22 -0500

    refactor(terraform): clean repo, remove .terraform artifacts, organize modules and pipeline

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,5 +1,7 @@
 variable "cluster_name" {}
 variable "vpc_id" {}
-variable "private_subnet_ids" {}
+variable "private_subnet_ids" {
+  type = list(string)
+}
 variable "cluster_role_arn" {}
 variable "node_role_arn" {}
